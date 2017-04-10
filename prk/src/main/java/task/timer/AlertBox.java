34497de5b24aa931d69b2aae4258d60748eb545a
Marcin <Marcin@172.20.48.69>
2017-04-10 19:17:46 +0200

    Pierwszy commit

---
+++ prk/src/main/java/task/timer/AlertBox.java
@@ -0,0 +1,29 @@
+package task.timer;
+
+
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
+public class AlertBox {
+
+	private AlertBox() {}
+
+	public static Optional<ButtonType> showAndWait(
+		AlertType alertType,
+		String title,
+		String content) {
+			Alert alert = new Alert(alertType);
+			
+			alert.setTitle(title);
+			alert.setHeaderText(null);
+			alert.setContentText(content);
+			alert.getDialogPane().setPrefWidth(600);
+			return 
+			alert.showAndWait();
+			}
+
+}
+
