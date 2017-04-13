646b907783449869f1228445f26b04f6a338d2ea
Mateusz <Mateusz@trybulec-hp>
2017-04-13 11:45:08 +0200

    Add schema database classes for tables (PROJECTS, RECORDS, USERS, TASKS)

---
+++ prk/src/main/java/database/Task.java
@@ -0,0 +1,20 @@
+package database;
+import java.sql.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="TASKS")
+
+public class Task {
+	@Id @GeneratedValue
+	@Column(name = "id")
+	 private int id;
+	
+	@Column(name="description")
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="record_id")
+	private Record record;
+}
