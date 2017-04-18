f20eb812b8195e7bc1185fc091821370f23a7b59
Marcin <Marcin@172.20.48.86>
2017-04-18 17:04:33 +0200

    add methods to show data; add method (mark) to read data from database (from table RECORDS)

--- prk/src/main/java/task/timer/view/RecordsController.java
+++ prk/src/main/java/task/timer/view/RecordsController.java
@@ -1,27 +1,29 @@
 package task.timer.view;
 
-
-
 import java.util.Calendar;
 
 import javax.swing.Timer;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import task.timer.model.Record;
+import task.timer.model.RecordToTableView;
 
 public class RecordsController {
-	@FXML private TableView<Record> recordTable;
-	@FXML private TableColumn<Record, Integer> userIdColumn;
-	@FXML private TableColumn<Record, String> projectNameColumn;
-	@FXML private TableColumn<Record, String> workNameColumn;
-	@FXML private TableColumn<Record, Long> startTimeColumn;
-	@FXML private TableColumn<Record, Long> stopTimeColumn;
+	@FXML private TableView<RecordToTableView> recordTable;
+	@FXML private TableColumn<RecordToTableView, Integer> userIdColumn;
+	@FXML private TableColumn<RecordToTableView, String> projectNameColumn;
+	@FXML private TableColumn<RecordToTableView, String> workNameColumn;
+	@FXML private TableColumn<RecordToTableView, Long> startTimeColumn;
+	@FXML private TableColumn<RecordToTableView, Long> stopTimeColumn;
 	
 	@FXML private Button startStopTime;
 	@FXML private Label measuredTime;
@@ -30,23 +32,10 @@ public class RecordsController {
 	private int timeCounter;
 	private Timer time;
 	
+	private final ObservableList<RecordToTableView> dataRecords = 
+			FXCollections.observableArrayList();
 	
-	private void startTimeMeaserement(Label measuredTimeLabel){
-		time = new Timer(1000, e -> {				
-			timeCounter++;
-			int timeHour = timeCounter/3600;
-			int rest = timeCounter%3600;										
-			int timeMinutes = rest/60;
-			int timeSeconds = rest%60;			
-			Platform.runLater(() -> measuredTime.setText(timeHour + ":" + timeMinutes + ":" + timeSeconds));
-		});		
-		time.start();
-	}
-	
-	private void stopTimeMeaserement(){
-		time.stop();
-	}
-	
+		
 	@FXML private void timing(){
 		if (startStopTime.getText().equals("START")){
 			startStopTime.setText("STOP");	
@@ -60,20 +49,59 @@ public class RecordsController {
 			startStopTime.setText("START");
 			stopTimeMeaserement();
 			projectChoice.setDisable(false);
+			
+
 		}
 	}
 
 @FXML private void initialize(){
-
 	measuredTime.setText("0:0:0");
 	
-	//recordTable.setTableMenuButtonVisible(true);
+	projectNameColumn.setCellValueFactory(
+			new PropertyValueFactory<>("projectName"));
 	
-	/*	userIdColumn.setCellValueFactory(cellData -> 
-			cellData.getValue().recordIdProperty().asObject());
+	workNameColumn.setCellValueFactory(
+			new PropertyValueFactory<>("workName"));
 	
-	projectNameColumn.setCellValueFactory(cellData ->
-			cellData.getValue().projectNameProperty());*/
+	startTimeColumn.setCellValueFactory(
+			new PropertyValueFactory<>("timeStartRecord"));
+	
+	stopTimeColumn.setCellValueFactory(
+			new PropertyValueFactory<>("timeStopRecord"));
+	
+	readProjectsFromDataBase();
+	
+	readRecordsFromDataBase();
+	
+	recordTable.setItems(dataRecords);
+
 }
+
+	private void startTimeMeaserement(Label measuredTimeLabel){
+		time = new Timer(1000, e -> {				
+			timeCounter++;
+			int timeHour = timeCounter/3600;
+			int rest = timeCounter%3600;										
+			int timeMinutes = rest/60;
+			int timeSeconds = rest%60;			
+			Platform.runLater(() -> measuredTime.setText(timeHour + ":" + timeMinutes + ":" + timeSeconds));
+		});		
+		time.start();
+	}
+
+	private void stopTimeMeaserement(){
+		time.stop();
+	}
+
+	private void readProjectsFromDataBase(){
+		
+	}
+	
+	private void readRecordsFromDataBase(){
+		dataRecords.add(new RecordToTableView("aa1", "bb1", "cc1", "stop1"));
+		dataRecords.add(new RecordToTableView("aa2", "bb2", "cc2", "stop2"));
+		dataRecords.add(new RecordToTableView("trzeci projekt", "trzecie zadanie", "czas", "stop3"));
+		dataRecords.add(new RecordToTableView("czwarty", "czwarte", "późno", "stop4"));
+	}
 
 }
