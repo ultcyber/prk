7ff9711f3f358c8451fcb8216c8a4de9b27751f9
Marcin <Marcin@172.20.48.86>
2017-04-19 18:49:03 +0200

    Add controllers to new fxmls file.

---
+++ prk/src/main/java/task/timer/view/ProjectController.java
@@ -0,0 +1,11 @@
+package task.timer.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ProjectController {
+	@FXML private TextField projectName;
+	@FXML private Button Dodaj;
+	
+}
