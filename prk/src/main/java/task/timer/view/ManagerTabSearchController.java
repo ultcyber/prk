a8334a488f2d4626a1030e93326b58c7dcf72421
Marcin <Marcin@MMZ.home>
2017-05-14 01:08:50 +0200

    added choose user and project

--- prk/src/main/java/task/timer/view/ManagerTabSearchController.java
+++ prk/src/main/java/task/timer/view/ManagerTabSearchController.java
@@ -1,5 +1,50 @@
 package task.timer.view;
 
-public class ManagerTabSearchController {
+import java.util.LinkedList;
+import java.util.List;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import task.timer.model.AbstractEntity;
+import task.timer.model.MEFactory;
+import task.timer.model.ManageEntity;
+import task.timer.model.Project;
+import task.timer.model.User;
+
+public class ManagerTabSearchController {
+	ManageEntity MMUser = new MEFactory().getUserEntityManager();
+	ManageEntity MMProject = new MEFactory().getProjectEntityManager();
+	
+	@FXML private ChoiceBox chooseUser;
+	@FXML private ChoiceBox chooseProject;
+
+	@FXML private void initialize(){
+		
+		
+		chooseUser.getItems().addAll(readUsersFromDataBase());
+		chooseProject.getItems().addAll(readProjectsFromDataBase());
+
+	}
+	
+	private List<String> readUsersFromDataBase(){
+		List<String> listUsers;
+		listUsers = new LinkedList<String>();		
+		List<AbstractEntity> users = MMUser.list();	
+		for (int i=0; i<users.size(); i++){
+			User userFromDb =   (User) users.get(i);		
+			listUsers.add(userFromDb.getFirstName()+ " " + userFromDb.getLastName());
+		}		
+		return listUsers;
+	}
+	
+	private List<String> readProjectsFromDataBase(){
+		List<String> listProjects;
+		listProjects = new LinkedList<String>();		
+		List<AbstractEntity> projects = MMProject.list();	
+		for (int i=0; i<projects.size(); i++){
+			Project projectFromDb =   (Project) projects.get(i);		
+			listProjects.add(projectFromDb.getName());
+		}		
+		return listProjects;
+	}
 }
