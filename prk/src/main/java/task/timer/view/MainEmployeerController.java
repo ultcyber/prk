59a5982caf959324d09f99134ac80e0b4650e0d6
Marcin <Marcin@172.20.48.116>
2017-05-18 19:41:31 +0200

    add methods to show records, add records and measure time depend on Local time

--- prk/src/main/java/task/timer/view/MainEmployeerController.java
+++ prk/src/main/java/task/timer/view/MainEmployeerController.java
@@ -1,95 +1,190 @@
 package task.timer.view;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.Timer;
 
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.StringConverter;
+import task.timer.model.AbstractEntity;
+import task.timer.model.MEFactory;
+import task.timer.model.ManageEntity;
 import task.timer.model.Project;
 import task.timer.model.Record;
-import task.timer.model.RecordFx;
+import task.timer.model.User;
 
 public class MainEmployeerController {
-	@FXML private TableView<RecordFx> recordTable;
-	@FXML private TableColumn<RecordFx, Integer> userIdColumn;
-	@FXML private TableColumn<RecordFx, String> projectNameColumn;
-	@FXML private TableColumn<RecordFx, String> workNameColumn;
-	@FXML private TableColumn<RecordFx, Long> startTimeColumn;
-	@FXML private TableColumn<RecordFx, Long> stopTimeColumn;
+	ManageEntity MMProject = new MEFactory().getProjectEntityManager();
+	ManageEntity MMRecord = new MEFactory().getRecordEntityManager();
+	ManageEntity MMUser = new MEFactory().getUserEntityManager();
+	
+	@FXML private TableView<Record> recordTable;
+	@FXML private TableColumn<Record, String> dateColumn;
+	@FXML private TableColumn<Record, String> projectNameColumn;
+	@FXML private TableColumn<Record, String> workNameColumn;
+	@FXML private TableColumn<Record, String> startTimeColumn;
+	@FXML private TableColumn<Record, String> stopTimeColumn;
 	
 	@FXML private Button startStopTime;
-	@FXML private Label measuredTime;
-	@FXML private ComboBox<String> projectChoice;
+	@FXML private Label measuredTimeHoursLabel;
+	@FXML private Label measuredTimeMinutesLabel;
+	@FXML private Label measuredTimeSecondsLabel;
+	@FXML private ChoiceBox projectChoice;
 	@FXML private TextField taskName;
+	@FXML private DatePicker date;
 	
-	private int timeCounter;
+	private final String pattern = "yyyy-MM-dd";
+	private LocalDate currentDate;
+	private LocalTime timeStart;
+	private LocalTime timeStop;
 	private Timer time;
+	private long passageOfTimeSeconds;		
+	private long timeHour;								
+	private long timeMinutes;
+	private long timeSeconds;
+	private long rest;	
+	List<AbstractEntity> projects;
+	List<AbstractEntity> users;
+	private int indexOfCurrentProject;
+	User loggedUser;
 	
-	private final ObservableList<RecordFx> dataRecords = 
+	private final ObservableList<Record> dataRecords = 
 			FXCollections.observableArrayList();
 	private final ObservableList<Project> dataProjects =
 			FXCollections.observableArrayList();
 	
 		
-	@FXML private void timing(){
+	@FXML private void timing() throws ClassNotFoundException{
 		if (startStopTime.getText().equals("START")){
-			startStopTime.setText("STOP");	
-			projectChoice.setDisable(true);
-			taskName.setDisable(true);
-			timeCounter=0;
-			measuredTime.setText("0:0:0");			
-			startTimeMeaserement(measuredTime);		
+			setVisualAfterStartButton();						
+			timeStart = LocalTime.now();
+			startTimeMeaserement();			
+			addNewRecordToDataBase();	
+			readAndShowRecordsFromDataBase();		
 		}				
 		else {
 			startStopTime.setText("START");
 			stopTimeMeaserement();
 			projectChoice.setDisable(false);
 			taskName.setDisable(false);
+			date.setDisable(false);
 		}
 	}
 
 @FXML private void initialize(){
-	measuredTime.setText("0:0:0");
+	loggedUser = setLoggedUser();
 	
-	projectNameColumn.setCellValueFactory(
-			new PropertyValueFactory<>("projectName"));
+	setDatePicker();
+	currentDate = date.getValue();	
 	
-	workNameColumn.setCellValueFactory(
-			new PropertyValueFactory<>("workName"));
+	dateColumn.setCellValueFactory(cellData ->
+		cellData.getValue().getDateProperty());
+
+	projectNameColumn.setCellValueFactory(cellData ->
+		cellData.getValue().getProjectName());
+		
+	//workNameColumn.setCellValueFactory(cellData ->
+	//	cellData.getValue().get);
 	
-	startTimeColumn.setCellValueFactory(
-			new PropertyValueFactory<>("timeStartRecord"));
+	startTimeColumn.setCellValueFactory(cellData ->
+		cellData.getValue().getTimeStartProperty());
 	
 	stopTimeColumn.setCellValueFactory(
 			new PropertyValueFactory<>("timeStopRecord"));
 	
-	//readProjectsFromDataBase();
-	//projectChoice.setItems(dataProjects);
 	
-	readRecordsFromDataBase();
-	recordTable.setItems(dataRecords);
+    projectChoice.setItems(readProjectsFromDataBase());
+
+    
+    readAndShowRecordsFromDataBase();
+	//recordTable.setItems(dataRecords);
 
 }
+	private void setVisualAfterStartButton(){
+		startStopTime.setText("STOP");	
+		projectChoice.setDisable(true);
+		taskName.setDisable(true);
+		date.setDisable(true);		
+		measuredTimeHoursLabel.setText("0");	
+		measuredTimeMinutesLabel.setText("0");
+		measuredTimeSecondsLabel.setText("0");
+	}
+	
+	private void addNewRecordToDataBase(){
+		indexOfCurrentProject = projectChoice.getSelectionModel().getSelectedIndex();	    
+	    Project currentProject = (Project)projects.get(indexOfCurrentProject);	    
+	    Integer recordID = MMRecord.add(
+	    		new Record(
+	    				loggedUser, 
+	    				currentProject, 
+	    				currentDate,
+	    				timeStart,
+	    				timeStart
+	    				));
+	}
 
-	private void startTimeMeaserement(Label measuredTimeLabel){
-		time = new Timer(1000, e -> {				
-			timeCounter++;
-			int timeHour = timeCounter/3600;
-			int rest = timeCounter%3600;										
-			int timeMinutes = rest/60;
-			int timeSeconds = rest%60;			
-			Platform.runLater(() -> measuredTime.setText(timeHour + ":" + timeMinutes + ":" + timeSeconds));
+	private User setLoggedUser(){
+		users = MMUser.list();	 
+		return (User) users.get(1);
+	}
+
+	private void setDatePicker(){
+		date.setValue(LocalDate.now());
+		StringConverter converter = new StringConverter<LocalDate>() {
+	        DateTimeFormatter dateFormatter = 
+	            DateTimeFormatter.ofPattern(pattern);
+	        
+	        @Override
+	        public String toString(LocalDate date) {
+	            if (date != null) 
+	            	return dateFormatter.format(date);
+	            else 
+	                return "";          
+	        }
+	        
+	        @Override
+	        public LocalDate fromString(String string) {
+	            if (string != null && !string.isEmpty()) 
+	                return LocalDate.parse(string, dateFormatter);
+	            else 
+	                return null;          
+	        }
+	    };             
+	    date.setConverter(converter);
+	    date.setPromptText(pattern.toLowerCase());
+	}
+
+	private void startTimeMeaserement(){
+		time = new Timer(1000, e -> {	
+			timeStop = LocalTime.now();
+			passageOfTimeSeconds = ChronoUnit.SECONDS.between(timeStart, timeStop);		
+			timeHour = passageOfTimeSeconds/3600;
+			rest = passageOfTimeSeconds%3600;										
+			timeMinutes = rest/60;
+			timeSeconds = rest%60;	
+			Platform.runLater(() -> measuredTimeHoursLabel.setText(timeHour + ""));
+			Platform.runLater(() -> measuredTimeMinutesLabel.setText(timeMinutes + ""));
+			Platform.runLater(() -> measuredTimeSecondsLabel.setText(timeSeconds + ""));		
 		});		
 		time.start();
 	}
@@ -98,15 +193,25 @@ public class MainEmployeerController {
 		time.stop();
 	}
 
-	private void readProjectsFromDataBase(){
-
+	private ObservableList<String> readProjectsFromDataBase(){
+		ObservableList<String> listProjectsName = FXCollections.observableArrayList();	
+		projects = MMProject.list();	
+		for (int i=0; i<projects.size(); i++){
+			Project projectFromDb =   (Project) projects.get(i);		
+			listProjectsName.add(projectFromDb.getName());
+		}		
+		return listProjectsName;
 	}
 	
-	private void readRecordsFromDataBase(){
-		dataRecords.add(new RecordFx("aa1", "bb1", "cc1", "stop1"));
-		dataRecords.add(new RecordFx("aa2", "bb2", "cc2", "stop2"));
-		dataRecords.add(new RecordFx("trzeci projekt", "trzecie zadanie", "czas", "stop3"));
-		dataRecords.add(new RecordFx("czwarty", "czwarte", "późno", "stop4"));
+	private void readAndShowRecordsFromDataBase(){
+		List<AbstractEntity> records; 	
+		records = MMRecord.list();			
+		dataRecords.clear();
+		for (int i=0; i<records.size(); i++){				
+			Record recordFromDb =   (Record) records.get(i);	
+			dataRecords.add(recordFromDb);					
+		}		
+		recordTable.setItems(dataRecords);	
 	}
 
 }
