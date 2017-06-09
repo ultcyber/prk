b2f83065342cf190e1598ebd71622fca73ddeac9
Marcin <Marcin@172.20.48.139>
2017-06-09 16:32:16 +0200

    added method: "toString"

--- prk/src/main/java/task/timer/model/Project.java
+++ prk/src/main/java/task/timer/model/Project.java
@@ -155,5 +155,11 @@ public class Project extends AbstractEntity {
 		this.users = users;
 	}
 	
-
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString(){
+		return name;
+	}
 }
